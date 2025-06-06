3c116d926ccdaf1600c3d18549c346718c975fbc
Shravan Chandra Parikipandla <sravanchandra74@gmail.com>
2025-06-06 09:36:31 +0300

    test commit

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,6 +1,6 @@
-output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
-}
+#output "cluster_endpoint" {
+#  value = module.eks.cluster_endpoint
+#}
 
 output "cluster_ca" {
   value = module.eks.cluster_certificate_authority_data
